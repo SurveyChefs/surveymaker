d8b754f27e0c3af73133cdd032655de016f51e94
ObyIslam <s00235259@atu.ie>
2025-02-19 21:55:09 +0000

    Bar chart css updates

--- src/app/components/SurveyBarChart.tsx
+++ src/app/components/SurveyBarChart.tsx
@@ -1,60 +1,83 @@
 import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const SurveyBarChart = ({ data }: { data: { [key: string]: number } }) => {
+  const answers = Object.keys(data);
+
+  const backgroundColors = [
+    "rgba(75, 192, 192, 0.2)",
+    "rgba(0, 255, 0, 0.24)",
+    "rgba(53, 162, 235, 0.2)",
+    "rgba(209, 10, 192, 0.2)",
+    "rgba(218, 221, 21, 0.2)",
+  ];
+  const borderColors = [
+    "rgba(75, 192, 192, 1)",
+    "rgb(9, 255, 83)",
+    "rgba(53, 162, 235, 1)",
+    "rgb(250, 18, 231)",
+    "rgb(229, 255, 0)",
+  ];
+
   const chartData = {
-    labels: Object.keys(data),
-    datasets: [
-      {
-        label: "Survey Results",
-        data: Object.values(data),
-        backgroundColor: [
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(0, 255, 0, 0.24)",
-          "rgba(53, 162, 235, 0.2)",
-          "rgba(209, 10, 192, 0.2)",
-          "rgba(218, 221, 21, 0.2)"
-        ],
-        borderColor: [
-          "rgba(75, 192, 192, 1)",
-          "rgb(9, 255, 83)",
-          "rgba(53, 162, 235, 1)",
-          "rgb(250, 18, 231)",
-          "rgb(229, 255, 0)"
-        ],
-        borderWidth: 1
-      }
-    ]
+    labels: ["Answers"], 
+    datasets: answers.map((answer, index) => ({
+      label: answer,
+      data: [data[answer]],
+      backgroundColor: backgroundColors[index % backgroundColors.length],
+      borderColor: borderColors[index % borderColors.length],
+      borderWidth: 1,
+    })),
   };
 
   const options = {
     responsive: true,
     plugins: {
+    
+      tooltip: {
+        callbacks: {
+          title: () => "", 
+          label: (context: any) => {
+
+            const label = context.dataset.label || "";
+            const value = context.raw;
+            return `${label}: ${value}`;
+          },
+        },
+      },
       legend: {
         labels: {
-          color: "white"
-        }
+          color: "white",
+        },
       },
       title: {
         display: true,
         text: "Survey Bar Chart",
-        color: "white"
-      }
+        color: "white",
+      },
     },
     scales: {
       x: {
         ticks: {
-          color: "white"
-        }
+          color: "white",
+        },
       },
       y: {
         ticks: {
-          color: "white"
-        }
-      }
-    }
+          color: "white",
+        },
+      },
+    },
   };
 
   return <Bar data={chartData} options={options} />;
