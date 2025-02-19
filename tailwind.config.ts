38d51b55ed2ba7f1be074834def1e3d5353ed5f6
James Mccafferty Devers - s00236260 <s00236260@atu.ie>
2025-02-19 21:52:42 +0000

    trying to fix css/tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,6 @@
-// tailwind.config.js
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        purple: {
-          600: "#5941BE",
-          700: "#453394",
-        },
-        gray: {
-          700: "#2D3748",
-          800: "#1A202C",
-          900: "#0a0a0a",
-        },
-      },
-      fontFamily: {
-        lato: ["Lato", "sans-serif"],
-      },
-    },
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
-} satisfies Config;
+};
